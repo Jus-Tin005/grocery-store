0f200151a11e9a5a2c8c4b7f36643e4aa287bd49
Khun Tun(Jus Tin) <jajahuge890@gmail.com>
2022-08-05 16:33:24 +0630

    evening on Aug 5

---
+++ src/Data.js
@@ -0,0 +1,78 @@
+
+
+
+let shopItemDatas = [{
+        id:"firstitem",
+        name: "Casaul Shirt",
+        price: 45,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "assets/img/angel.jpg"
+},{
+        id:"seconditem",
+        name: "Office Shirt",
+        price: 50,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "assets/img/smartlady.jpg"
+},{
+        id:"thirditem",
+        name: "Comfortable Shirt",
+        price: 75,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "assets/img/teenagerwear.jpg"
+},{
+        id:"fourthitem",
+        name: "Travelling Shirt",
+        price: 100,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "assets/img/glasses.jpg"
+},{
+        id:"fifthitem",
+        name: "Famous Shoes",
+        price: 150,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "assets/img/blackshoes.jpg"
+},{
+        id:"sixthitem",
+        name: "Famous Hair",
+        price: 120,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "assets/img/brownhair.jpg"
+},{
+        id:"seventhitem",
+        name: "Famous T-shirt",
+        price: 110,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "assets/img/younggirl.jpg"
+},{
+        id:"eightitem",
+        name: "Famous Men T-shirt",
+        price: 130,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "assets/img/men.jpg"
+},{
+
+        id:"ninethitem",
+        name: "Smart Suit",
+        price: 140,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "assets/img/flightattantdant.jpg"
+},{
+
+        id:"tenthitem",
+        name: "Light Suit",
+        price: 145,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "assets/img/tallgirl.jpg"
+},{
+        id:"eleventhitem",
+        name: "Wedding Suit",
+        price: 2000,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "assets/img/beautifullady.jpg"
+},{
+        id:"twethitem",
+        name: "Rainy Suit",
+        price: 145,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        img: "assets/img/longhair.jpg"
+}];
